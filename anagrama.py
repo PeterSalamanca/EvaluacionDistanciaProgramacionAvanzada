bec15e1aa056a4c7679fa914f23a55b41a1b6367
PeterSalamanca <113950094+PeterSalamanca@users.noreply.github.com>
2023-06-03 20:23:49 -0500

    Update anagrama.py

--- anagrama.py
+++ anagrama.py
@@ -19,3 +19,38 @@ print(anagrama_2("mora", "ramo"))  # True
 print(anagrama_2("casa", "saca"))  # True
 print(anagrama_2("Cazar", "raza"))  # False
 
+# Código corregido para analizar una frase y encontrar los anagramas 
+
+from collections import Counter
+
+def es_anagrama(palabra1, palabra2):
+    # Convierte las palabras en contadores de caracteres
+    contador1 = Counter(palabra1.lower())
+    contador2 = Counter(palabra2.lower())
+
+    # Compara los contadores para determinar si son anagramas
+    if contador1 == contador2:
+        return True
+    else:
+        return False
+
+def encontrar_anagramas(frase):
+    palabras = frase.split()
+    anagramas = []
+
+    # Compara cada par de palabras en la frase para encontrar anagramas
+    for i in range(len(palabras)):
+        for j in range(i + 1, len(palabras)):
+            if es_anagrama(palabras[i], palabras[j]):
+                anagramas.append((palabras[i], palabras[j]))
+
+    return anagramas
+
+# Ejemplo de uso
+frase = "Una persona se desea casar en roma sin importar su dinero, solo busca un gran amor y propone a sacar el anillo de matrimonio"
+resultado = encontrar_anagramas(frase)
+print("Anagramas encontrados:")
+for par in resultado:
+    print(f"{par[0]} - {par[1]}")
+
+
