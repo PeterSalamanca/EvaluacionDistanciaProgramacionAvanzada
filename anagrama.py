d43cb82043c2964fa243e703ee8c4d4ddb96d923
PeterSalamanca <113950094+PeterSalamanca@users.noreply.github.com>
2023-06-03 19:59:41 -0500

    Update anagrama.py

--- anagrama.py
+++ anagrama.py
@@ -3,7 +3,19 @@ def anagrama(strg1, strg2):
     # retornará una nueva lista ordenada de cada cadena
     # Confirmando si la primera y segunda cadena contienen los mismos caracteres
 
-print(anagrama("roma","amor"))
-print(anagrama("hola","adios"))
+print(anagrama("roma", "amor"))  # True
+print(anagrama("hola", "adios"))  # False
+
+
+# Código corregido
+
+def anagrama_2(strg1, strg2):
+    strg1 = strg1.lower()
+    strg2 = strg2.lower()
+    return sorted(strg1) == sorted(strg2)
+
+print(anagrama_2("saco", "cosa"))  # True
+print(anagrama_2("mora", "ramo"))  # True
+print(anagrama_2("casa", "saca"))  # True
 
 
